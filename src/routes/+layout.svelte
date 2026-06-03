b7d6cfd96bc0fcfb7f83703fdaf021c636a9978a
Ismail Zouaoui <ishmael.zouaoui@gmail.com>
2026-06-04 00:05:50 +0100

    Re-setup the project with with updated Sveltekit template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,9 @@
 <script lang="ts">
-	import '../app.css';
-	import { page } from '$app/stores';
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
 </script>
 
-<div class="bg-background min-h-screen">
-	<header class="mx-auto max-w-2xl px-4 py-8">
-		<nav>
-			<div class="flex items-center justify-between">
-				<a href="/" class="text-accent font-mono font-medium">Ismail Zouaoui</a>
-
-				<div class="flex space-x-6">
-					<a href="/blog" class="nav-link" class:active={$page.url.pathname === '/blog'}>Writing</a>
-					<a href="/about" class="nav-link" class:active={$page.url.pathname === '/about'}>About</a>
-				</div>
-			</div>
-		</nav>
-	</header>
-
-	<main class="px-4 py-12">
-		{@render children()}
-	</main>
-</div>
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
